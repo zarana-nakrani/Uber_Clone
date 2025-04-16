d011273ba4eb0b8027d9e4210733022d36d4db56
zarana-nakrani <zarnasnakrani17@gmail.com>
2025-04-16 08:20:56 -0700

    first commit

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+const dotenv = require('dotenv')
+dotenv.config();
+const express = require('express')
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+module.exports = app;
